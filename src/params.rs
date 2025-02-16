38bb65140b9185eb98f01ef4d8c85675b892ccf0
ProAlize <1317460037@qq.com>
2025-02-16 21:37:03 +0800

    7

--- src/params.rs
+++ src/params.rs
@@ -35,19 +35,22 @@ impl LLamaParams<f32> {
 }
 */
 impl LLamaParams<f32> {
-    pub fn from_safetensors(safetensor: &SafeTensors, config: &LlamaConfigJson) -> Self {
-        let get_tensor = |name: &str| -> Tensor<f32> {
-            let tensor = safetensor.tensor(name).unwrap_or_else(|_| panic!("Tensor {} not found", name));
-            let data = tensor.data().to_vec();
-            let data_f32: Vec<f32> = data.chunks_exact(4).map(|chunk| {
-                f32::from_le_bytes([chunk[0], chunk[1], chunk[2], chunk[3]]).clone()
-            }).collect();
-            let shape = tensor.shape().to_vec();
-            Tensor::new(data_f32, &shape)
+    pub fn from_safetensors(safetensor: &SafeTensors, config: &LlamaConfigJson) -> Result<Self, String> {
+        let get_tensor = |name: &str| -> Result<Tensor<f32>, String> {
+            match safetensor.tensor(name) {
+                Ok(tensor) => {
+                    let data = tensor.data().to_vec();
+                    let data_f32: Vec<f32> = data.chunks_exact(4).map(|chunk| {
+                        f32::from_le_bytes([chunk[0], chunk[1], chunk[2], chunk[3]])
+                    }).collect();
+                    let shape = tensor.shape().to_vec();
+                    Ok(Tensor::new(data_f32, &shape))
+                }
+                Err(_) => Err(format!("Tensor {} not found", name)),
+            }
         };
 
         let n_layers = config.num_hidden_layers;
-
         let mut rms_att_w = Vec::with_capacity(n_layers);
         let mut wq = Vec::with_capacity(n_layers);
         let mut wk = Vec::with_capacity(n_layers);
@@ -59,26 +62,30 @@ impl LLamaParams<f32> {
         let mut w_down = Vec::with_capacity(n_layers);
 
         for layer in 0..n_layers {
-            rms_att_w.push(get_tensor(&format!("model.layers.{}.input_layernorm.weight", layer)));
-            wq.push(get_tensor(&format!("model.layers.{}.self_attn.q_proj.weight", layer)));
-            wk.push(get_tensor(&format!("model.layers.{}.self_attn.k_proj.weight", layer)));
-            wv.push(get_tensor(&format!("model.layers.{}.self_attn.v_proj.weight", layer)));
-            wo.push(get_tensor(&format!("model.layers.{}.self_attn.o_proj.weight", layer)));
-            rms_ffn_w.push(get_tensor(&format!("model.layers.{}.post_attention_layernorm.weight", layer)));
-            w_up.push(get_tensor(&format!("model.layers.{}.mlp.up_proj.weight", layer)));
-            w_gate.push(get_tensor(&format!("model.layers.{}.mlp.gate_proj.weight", layer)));
-            w_down.push(get_tensor(&format!("model.layers.{}.mlp.down_proj.weight", layer)));
+            rms_att_w.push(get_tensor(&format!("model.layers.{}.input_layernorm.weight", layer))?);
+            wq.push(get_tensor(&format!("model.layers.{}.self_attn.q_proj.weight", layer))?);
+            wk.push(get_tensor(&format!("model.layers.{}.self_attn.k_proj.weight", layer))?);
+            wv.push(get_tensor(&format!("model.layers.{}.self_attn.v_proj.weight", layer))?);
+            wo.push(get_tensor(&format!("model.layers.{}.self_attn.o_proj.weight", layer))?);
+            rms_ffn_w.push(get_tensor(&format!("model.layers.{}.post_attention_layernorm.weight", layer))?);
+            w_up.push(get_tensor(&format!("model.layers.{}.mlp.up_proj.weight", layer))?);
+            w_gate.push(get_tensor(&format!("model.layers.{}.mlp.gate_proj.weight", layer))?);
+            w_down.push(get_tensor(&format!("model.layers.{}.mlp.down_proj.weight", layer))?);
         }
 
-        let embedding_table = get_tensor("model.embed_tokens.weight");
+        let embedding_table = get_tensor("model.embed_tokens.weight")?;
+        
+        // 手动复制embedding_table
         let lm_head = if config.tie_word_embeddings {
-            embedding_table.clone()
+            // 直接创建一个新的Tensor对象作为lm_head
+            Tensor::new(embedding_table.data().to_vec(), &embedding_table.shape())
         } else {
-            get_tensor("lm_head.weight")
+            get_tensor("lm_head.weight")?
         };
-        let rms_out_w = get_tensor("model.norm.weight");
 
-        LLamaParams {
+        let rms_out_w = get_tensor("model.norm.weight")?;
+
+        Ok(LLamaParams {
             embedding_table,
             rms_att_w,
             wq,
@@ -91,6 +98,6 @@ impl LLamaParams<f32> {
             w_down,
             rms_out_w,
             lm_head,
-        }
+        })
     }
 }
